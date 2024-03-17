e9f2583de4d4857954f0120d25fe5509b35f5aa0
Eliot Hennebo <eliot2090@gmail.com>
2024-03-17 13:02:20 +0100

    Integration with Directus and Cloudron

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import { createDirectus, authentication, rest, readItems, readItem, updateItem, login, readCollection, createItem } from '@directus/sdk';
+
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 const port = 3005;
@@ -10,142 +14,85 @@ app.use(bodyParser.json());
 
 app.use(express.static('../react/out'));
 
-const data = [
-  {
-    id: '1',
-    first_name: 'Robert',
-    last_name: 'Wolfkisser',
-    company: 'Wolfkisser Inc.',
-    job: 'Engineer',
-    email: 'rob_wolf@gmail.com',
-  },
-  {
-    id: '2',
-    first_name: 'Robert',
-    last_name: 'Wolfkisser',
-    company: 'Wolfkisser Inc.',
-    job: 'Engineer',
-    email: 'rob_wolf@gmail.com',
-  },
-  {
-    id: '3',
-    first_name: 'Robert',
-    last_name: 'Wolfkisser',
-    company: 'Wolfkisser Inc.',
-    job: 'Engineer',
-    email: 'rob_wolf@gmail.com',
-  },
-];
+
+const client = createDirectus('https://directus.cloudfest.dev/').with(rest()).with(authentication());
+await client.login('ssoapp@cloudfest.dev', 'aabbccddaabbccddaabbccdd');
+
+
+async function fetchParticipantsByStatus(status) {
+  try {
+    const filter = status ? { status: { _eq: status } } : {};
+    return await client.request(readItems('participants', { filter }));
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
 
 app.listen(port, () => {
-
-  app.get('/api/users/', async (req, res) => {
+  app.get('/api/users/:status?', async (req, res) => {
+    const { status } = req.params;
+    
     try {
-      return res.json({ user: data });
+      const result = await fetchParticipantsByStatus(status);
+      res.json({ users: result });
     } catch (err) {
       res.status(500).send(err.message);
     }
   });
+  
 
-  app.get('/api/users/pending', async (req, res) => {
-    try {
-      return res.json({ user: data });
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
-
-  app.get('/api/users/approved', async (req, res) => {
-    try {
-      return res.json({ user: data });
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
-
-  app.get('/api/users/rejected', async (req, res) => {
-    try {
-      return res.json({ user: data });
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
-
-  app.put('/api/users/approve/:id', async (req, res) => {
+  app.put('/api/users/:status?/:id', async (req, res) => {
     const { id } = req.params;
+    const { status } = req.params;
+
     try {
-      res.send({ response: 'User approved' });
+      const result = await client.request(updateItem('participants', id, { status: status}))
+      
+      if (status === 'approved') {
+        const user = await client.request(readItem('participants', id));
+
+        const cloudronAddUserResult = await axios.post('https://my.cloudfest.dev/api/v1/users', 
+          {
+            email: user.email,
+            fallbackEmail: "",
+            displayName: (user.firstname ? user.firstname : '') + (user.lastname ? ' '+user.lastname : ''), 
+            role: user.role
+          }, 
+          { headers: { 'Authorization': 'Bearer 53c0c88fb22668e1c81ea328df6af94c3f9d8a595f1fa97c50c9cd62a0c421a0', 'Content-Type': 'application/json;charset=UTF-8' } }
+        );
+
+        console.log(cloudronAddUserResult.data.id);
+
+        await axios.post('https://my.cloudfest.dev/api/v1/users/'+ cloudronAddUserResult.data.id +'/send_invite_email', 
+          { email: user.email }, 
+          { headers: { 'Authorization': 'Bearer 53c0c88fb22668e1c81ea328df6af94c3f9d8a595f1fa97c50c9cd62a0c421a0', 'Content-Type': 'application/json;charset=UTF-8' } }
+        );
+
+
+      }
+
+      res.send(
+        { 
+          response: result,
+          newStatus: status
+        });
     } catch (err) {
       res.status(500).send(err.message);
     }
   });
 
-  app.put('/api/users/reject/:id', async (req, res) => {
-    const { id } = req.params;
+  app.put('/api/users', async (req, res) => {
+    const user = req.body;
+
     try {
-      res.send({ response: 'User rejected' });
+      const result = await client.request(createItem('participants', 
+        user
+      ));
+
+      res.json({ users: result });
     } catch (err) {
       res.status(500).send(err.message);
     }
   });
-
-
-  // app.get('/api/users/pending', async (req, res) => {
-  //   try {
-  //     const result = await pool.query('SELECT * FROM users WHERE status = $1', ['PENDING']);
-  //     res.json(result.rows);
-  //   } catch (err) {
-  //     res.status(500).send(err.message);
-  //   }
-  // });
-
-  // app.get('/api/users/approved', async (req, res) => {
-  //   try {
-  //     const result = await pool.query('SELECT * FROM users WHERE status = $1', ['APPROVED']);
-  //     res.json(result.rows);
-  //   } catch (err) {
-  //     res.status(500).send(err.message);
-  //   }
-  // });
-
-  // app.get('/api/users/rejected', async (req, res) => {
-  //   try {
-  //     const result = await pool.query('SELECT * FROM users WHERE status = $1', ['REJECTED']);
-  //     res.json(result.rows);
-  //   } catch (err) {
-  //     res.status(500).send(err.message);
-  //   }
-  // });
-
-  // app.post('/api/users', async (req, res) => {
-  //   const { role, email, password } = req.body;
-  //   try {
-  //     await pool.query('INSERT INTO users (role, email, password, status) VALUES ($1, $2, $3, $4)', [role, email, password, 'PENDING']);
-  //     res.status(201).send('User created');
-  //   } catch (err) {
-  //     res.status(500).send(err.message);
-  //   }
-  // });
-
-  // app.put('/api/users/approve/:id', async (req, res) => {
-  //   const { id } = req.params;
-  //   try {
-  //     await pool.query('UPDATE users SET status = $1 WHERE id = $2', ['APPROVED', id]);
-  //     res.send('User approved');
-  //   } catch (err) {
-  //     res.status(500).send(err.message);
-  //   }
-  // });
-
-  // app.put('/api/users/reject/:id', async (req, res) => {
-  //   const { id } = req.params;
-  //   try {
-  //     await pool.query('UPDATE users SET status = $1 WHERE id = $2', ['REJECTED', id]);
-  //     res.send('User rejected');
-  //   } catch (err) {
-  //     res.status(500).send(err.message);
-  //   }
-  // });
-
-  console.log(`Server listening on ${port}`);
 });  
