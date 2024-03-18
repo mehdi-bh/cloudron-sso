654aaf5b4b599da61139ffef981fcec25c158905
Eliot Hennebo <eliot2090@gmail.com>
2024-03-18 11:33:11 +0100

    Add delete endpoint

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-import { createDirectus, authentication, rest, readItems, readItem, updateItem, login, readCollection, createItem } from '@directus/sdk';
+import { createDirectus, authentication, rest, readItems, readItem, updateItem, deleteItems, deleteItem, login, readCollection, createItem } from '@directus/sdk';
 
 
 import express from 'express';
@@ -16,8 +16,9 @@ app.use(express.static('../react/out'));
 
 
 const client = createDirectus('https://directus.cloudfest.dev/').with(rest()).with(authentication());
-await client.login('ssoapp@cloudfest.dev', 'aabbccddaabbccddaabbccdd');
+// await client.login('ssoapp@cloudfest.dev', 'aabbccddaabbccddaabbccdd');
 
+await client.login('backend@gmail.com', 'backend12345');
 
 async function fetchParticipantsByStatus(status) {
   try {
@@ -32,7 +33,7 @@ async function fetchParticipantsByStatus(status) {
 app.listen(port, () => {
   app.get('/api/users/:status?', async (req, res) => {
     const { status } = req.params;
-
+    
     try {
       const result = await fetchParticipantsByStatus(status);
       res.json({ users: result });
@@ -40,7 +41,7 @@ app.listen(port, () => {
       res.status(500).send(err.message);
     }
   });
-
+  
 
   app.put('/api/users/:status?/:id', async (req, res) => {
     const { id } = req.params;
@@ -48,62 +49,73 @@ app.listen(port, () => {
 
     try {
       const result = await client.request(updateItem('participants', id, { status: status}))
-
+      
       if (status === 'approved') {
         const user = await client.request(readItem('participants', id));
-        console.log('user ' + user)
-        const cloudronAddUserResult = await axios.post('https://my.cloudfest.dev/api/v1/users',
+
+        const cloudronAddUserResult = await axios.post('https://my.cloudfest.dev/api/v1/users', 
           {
             email: user.email,
             fallbackEmail: "",
-            displayName: (user.firstname ? user.firstname : '') + (user.lastname ? ' ' + user.lastname : ''),
+            displayName: (user.firstname ? user.firstname : '') + (user.lastname ? ' '+user.lastname : ''), 
             role: user.role
-          },
-          { headers: { 'Authorization': 'Bearer 53c0c88fb22668e1c81ea328df6af94c3f9d8a595f1fa97c50c9cd62a0c421a0', 'Content-Type': 'application/json;charset=UTF-8' } }
-        );
-        console.log('cloudron added status ' + cloudronAddUserResult.status);
-        console.log('cloudron id ' + cloudronAddUserResult.data.id);
-
-        const email_result = await axios.post('https://my.cloudfest.dev/api/v1/users/' + cloudronAddUserResult.data.id + '/send_invite_email',
-          { email: user.email },
+          }, 
           { headers: { 'Authorization': 'Bearer 53c0c88fb22668e1c81ea328df6af94c3f9d8a595f1fa97c50c9cd62a0c421a0', 'Content-Type': 'application/json;charset=UTF-8' } }
         );
 
-        console.log('email sent ' + email_result);
+        console.log(cloudronAddUserResult.data.id);
 
+        await axios.post('https://my.cloudfest.dev/api/v1/users/'+ cloudronAddUserResult.data.id +'/send_invite_email', 
+          { email: user.email }, 
+          { headers: { 'Authorization': 'Bearer 53c0c88fb22668e1c81ea328df6af94c3f9d8a595f1fa97c50c9cd62a0c421a0', 'Content-Type': 'application/json;charset=UTF-8' } }
+        );
       }
 
       res.send(
-        {
+        { 
           response: result,
           newStatus: status
         });
     } catch (err) {
       res.status(500).send(err.message);
     }
-  });
+    });
 
   app.put('/api/users', async (req, res) => {
-    let user = req.body;
-    user.status = 'pending';
-    user.role = 'user';
-
-    const filter = user.email ? { email: { _eq: user.email } } : {};
-    const userExists = await client.request(readItems('participants', { filter }));
-
-    if (userExists.length > 0) {
-      res.status(501).send('Email already exists');
-      return;
-    }
+    const user = req.body;
 
     try {
-      const result = await client.request(createItem('participants',
+      const result = await client.request(createItem('participants', 
         user
       ));
 
       res.json({ users: result });
     } catch (err) {
       res.status(500).send(err.message);
+    }
+  });
+
+  app.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const result = await client.request(deleteItem('participants', id));
+      res.json({ response: result });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  });
+
+  app.delete('/api/users/', async (req, res) => {
+  
+    try {
+      const rejectedUsers = await fetchParticipantsByStatus('rejected');
+      const ids = rejectedUsers.map(user => user.id + '');
+
+      const result = await client.request(deleteItems('participants', { status: { _eq: 'rejected' } }));
+      res.json({ response: result });
+    } catch (err) {
+      res.status(500).send(err);
     }
   });
 });  
